7dbb13c668750412f2ac78547d717a147bfb862b
Manwë <manuel.etchegaray7@gmail.com>
2024-04-18 20:58:57 -0300

    move to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use sysinfo::{Pid as SysPid, System};
+
 pub mod constants;
 pub mod field_definition;
 pub mod managed;
@@ -7,6 +9,113 @@ pub mod pe_reader;
 pub mod type_code;
 pub mod type_definition;
 pub mod type_info;
+
+use managed::Managed;
+use mono_reader::MonoReader;
+use type_code::TypeCode;
+use type_definition::TypeDefinition;
+use type_info::TypeInfo;
+
+use napi_derive::napi;
+
+pub fn find_pid_by_name(name: &str) -> Option<SysPid> {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    sys.processes()
+        .iter()
+        .find(|(_, process)| process.name().contains(name))
+        .map(|(pid, _)| *pid)
+}
+
+pub fn get_def_by_name<'a>(
+    defs: &'a Vec<usize>,
+    name: &str,
+    mono_reader: &MonoReader,
+) -> Option<&'a usize> {
+    defs.iter().find(|def| {
+        let main_typedef = TypeDefinition::new(**def, &mono_reader);
+        main_typedef.name == name
+    })
+}
+
+#[napi]
+pub fn read_data(process_name: String, fields: Vec<&str>) -> String {
+    println!("Reading started...");
+
+    let pid = find_pid_by_name(&process_name);
+
+    if pid.is_none() {
+        return String::from("Process not found");
+    }
+
+    let mut return_string: String = String::new();
+
+    pid.iter().for_each(|pid| {
+        let mut mono_reader = MonoReader::new(pid.as_u32());
+
+        mono_reader.read_mono_root_domain();
+        mono_reader.read_assembly_image();
+        let defs = mono_reader.create_type_definitions();
+
+        // get the type defs on the root of the assembly for the first loop
+        let definition = get_def_by_name(&defs, fields[0], &mono_reader)
+            .unwrap()
+            .clone();
+
+        // skipt the first item in the find array
+        let find = &fields[1..];
+
+        let mut field = (definition.clone(), TypeInfo::new(definition, &mono_reader));
+
+        for (index, name) in find.iter().enumerate() {
+            field = match index {
+                0 => {
+                    let class = TypeDefinition::new(definition, &mono_reader);
+                    class.get_static_value(name)
+                }
+                _ => {
+                    let managed = Managed::new(&mono_reader, field.0, None);
+                    let ptr = mono_reader.read_ptr(field.0);
+                    let code = field.1.clone().code();
+                    let class = match code {
+                        TypeCode::GENERICINST => managed.read_generic_instance(field.1.clone()),
+                        _ => managed.read_class(),
+                    };
+                    class.get_value(name, ptr)
+                }
+            };
+            let code = field.1.clone();
+            println!("Find: {}: {} {}", name, code.code(), field.0);
+        }
+
+        let managed = Managed::new(&mono_reader, field.0, None);
+        let ptr = mono_reader.read_ptr(field.0);
+        let code = field.1.clone().code();
+
+        let strout = match code {
+            TypeCode::CLASS => {
+                let mut class = managed.read_class();
+                class.set_fields_base(ptr);
+                class.to_string()
+            }
+            TypeCode::GENERICINST => {
+                let mut class = managed.read_generic_instance(field.1.clone());
+                class.set_fields_base(ptr);
+                class.to_string()
+            }
+            TypeCode::SZARRAY => managed.read_managed_array(),
+            _ => {
+                println!("Code: {} strout not implemented", code);
+                String::from("{}")
+            }
+        };
+
+        return_string = strout.clone();
+    });
+
+    return_string
+}
 
 /*
 pub fn read_managed<T>(type_code: TypeCode) -> Option<T> {
--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,27 @@
-use sysinfo::{Pid as SysPid, System};
-
-use mtga_reader::managed::Managed;
-use mtga_reader::mono_reader::MonoReader;
-use mtga_reader::type_code::TypeCode;
-use mtga_reader::type_definition::TypeDefinition;
-use mtga_reader::type_info::TypeInfo;
-
-fn find_pid_by_name(name: &str) -> Option<SysPid> {
-    let mut sys = System::new_all();
-    sys.refresh_all();
-
-    sys.processes()
-        .iter()
-        .find(|(_, process)| process.name().contains(name))
-        .map(|(pid, _)| *pid)
-}
-
-fn get_def_by_name<'a>(
-    defs: &'a Vec<usize>,
-    name: &str,
-    mono_reader: &MonoReader,
-) -> Option<&'a usize> {
-    defs.iter().find(|def| {
-        let main_typedef = TypeDefinition::new(**def, &mono_reader);
-        main_typedef.name == name
-    })
-}
+use mtga_reader::read_data;
 
 fn main() {
-    println!("Reading started...");
+    // let find = [
+    //     "WrapperController",
+    //     "<Instance>k__BackingField",
+    //     "<InventoryManager>k__BackingField",
+    //     "_inventoryServiceWrapper",
+    //     "<Cards>k__BackingField",
+    //     "_entries",
+    // ];
+    
+    let find = vec![
+        "PAPA",
+        "_instance",
+        "_inventoryManager",
+        "_inventoryServiceWrapper",
+        "m_inventory",
+    ];
 
-    let pid = find_pid_by_name("MTGA");
+    let data = read_data(
+        "MTGA".to_string(),
+        find,
+    );
 
-    if pid.is_none() {
-        println!("MTGA not found");
-        return;
-    }
-
-    pid.iter().for_each(|pid| {
-        let mut mono_reader = MonoReader::new(pid.as_u32());
-
-        mono_reader.read_mono_root_domain();
-        mono_reader.read_assembly_image();
-        let defs = mono_reader.create_type_definitions();
-
-        let find = [
-            "PAPA",
-            "_instance",
-            "_inventoryManager",
-            "_inventoryServiceWrapper",
-            "m_inventory",
-        ];
-
-        // let find = [
-        //     "WrapperController",
-        //     "<Instance>k__BackingField",
-        //     "<InventoryManager>k__BackingField",
-        //     "_inventoryServiceWrapper",
-        //     "<Cards>k__BackingField",
-        //     "_entries",
-        // ];
-
-        // get the type defs on the root of the assembly for the first loop
-        let definition = get_def_by_name(&defs, find[0], &mono_reader)
-            .unwrap()
-            .clone();
-
-        // skipt the first item in the find array
-        let find = &find[1..];
-
-        let mut field = (definition.clone(), TypeInfo::new(definition, &mono_reader));
-
-        for (index, name) in find.iter().enumerate() {
-            field = match index {
-                0 => {
-                    let class = TypeDefinition::new(definition, &mono_reader);
-                    class.get_static_value(name)
-                }
-                _ => {
-                    let managed = Managed::new(&mono_reader, field.0, None);
-                    let ptr = mono_reader.read_ptr(field.0);
-                    let code = field.1.clone().code();
-                    let class = match code {
-                        TypeCode::GENERICINST => managed.read_generic_instance(field.1.clone()),
-                        _ => managed.read_class(),
-                    };
-                    class.get_value(name, ptr)
-                }
-            };
-            let code = field.1.clone();
-            println!("Find: {}: {} {}", name, code.code(), field.0);
-        }
-
-        let managed = Managed::new(&mono_reader, field.0, None);
-        let ptr = mono_reader.read_ptr(field.0);
-        let code = field.1.clone().code();
-
-        let strout = match code {
-            TypeCode::CLASS => {
-                let mut class = managed.read_class();
-                class.set_fields_base(ptr);
-                class.to_string()
-            }
-            TypeCode::GENERICINST => {
-                let mut class = managed.read_generic_instance(field.1.clone());
-                class.set_fields_base(ptr);
-                class.to_string()
-            }
-            TypeCode::SZARRAY => managed.read_managed_array(),
-            _ => {
-                println!("Code: {} strout not implemented", code);
-                String::from("{}")
-            }
-        };
-
-        println!("{}", strout);
-    });
+    println!("{}", data);
 }
